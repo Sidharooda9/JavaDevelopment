bde0d44183184731fda7566b417761b4d3505cbf
SIDDU\SIDDU <ksiddu0575@gmail.com>
2026-07-01 07:39:07 +0530

    First commit

--- src/main/java/com/example/accounts/controller/AccountController.java
+++ src/main/java/com/example/accounts/controller/AccountController.java
@@ -9,6 +9,7 @@ public class AccountController {
     @GetMapping("sayhello")
     public String sayHello(){
         System.out.println("siddu");
+        System.out.println("Ok");
         return "Hello World";
     }
 }
